9df6d5df1bd797232c4647565bd1c9e82b8e2dd9
Abhishek2122-star <abhisheklad963@gmail.com>
2026-07-03 01:58:33 +0530

    DSA Problem

---
+++ src/main/java/org/example/Starpattern/DSA/TwoSum.java
@@ -0,0 +1,23 @@
+package org.example.Starpattern.DSA;
+import java.util.*;
+
+public class TwoSum {
+    public static void main(String []args){
+        int [] arr = {2,3,4,5,6,7};
+        int target = 9;
+
+        for (int i = 0 ; i < arr.length ; i++){
+            for (int j = i + 1 ; j < arr.length ; j++){
+                if (arr[i] + arr[j] == target){
+                    System.out.println(i + ", " + j);
+                    return;
+                }
+
+
+            }
+
+        }
+
+
+    }
+}

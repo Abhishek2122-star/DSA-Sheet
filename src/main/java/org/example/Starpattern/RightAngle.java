f8aa94c062c10d4537a4b1015cb3f8dea0186552
Abhishek2122-star <abhisheklad963@gmail.com>
2026-07-02 14:52:13 +0530

    DSA Sheet

---
+++ src/main/java/org/example/Starpattern/RightAngle.java
@@ -0,0 +1,14 @@
+package org.example.Starpattern;
+
+public class RightAngle {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
